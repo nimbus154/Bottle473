9dc19ca05c1a71b687ecb8615933b8900949d236
David Robison <davidrobison122@hotmail.com>
2013-04-27 19:51:30 -0700

    Preappended /test on my test urls to not conflict with other parts of the project.  Hashing passwords now in sha224

--- users.py
+++ users.py
@@ -12,36 +12,41 @@ filepath = os.getcwd()
 
 bottle.TEMPLATE_PATH.insert(0, filepath + '/static/templates/')
 
-@route('/')
-@route('/users')
+@route('/test')
+@route('/test/users')
 def register():
     userList = (Users.objects)
     return template('users.tpl', rows=userList)
  
-@route('/register')
+@route('/test/register')
 def register():
     return template('register.tpl')
     
-@route('/register', method='POST')
+@route('/test/register', method='POST')
 def register_post():
+    
+    password = request.forms.get('pass', '').strip()
+    hashedpw = hashlib.sha224(password).hexdigest()
     newUser = Users()
     newUser.username = request.forms.get('user', '').strip()
-    newUser.password = request.forms.get('pass', '').strip()
+    newUser.password = hashedpw
     newUser.save()   
     redirect("/")
 
-@route('/login')
+@route('/test/login')
 def register():
     return template('login.tpl')
     
-@route('/login', method='POST')
+@route('/test/login', method='POST')
 def register_post():
     
     username = request.forms.get('user', '').strip()
     password = request.forms.get('pass', '').strip()
-    
+    hashedpw = hashlib.sha224(password).hexdigest()
     
     newUser = Users.objects(username = username)
-
-
-    return 'Your password is %s' % newUser[0].password
+    
+    if( newUser[0].password == hashedpw ):
+        return 'Success'
+    else:
+        return 'Failed'
