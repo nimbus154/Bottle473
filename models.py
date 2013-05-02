104578262b957908a6f08f0e45aff8e5c136d236
ZosoCoder <ZosoLzrd@gmail.com>
2013-05-01 20:07:49 -0700

    Added Course, Schedule, & Department to models.py

--- models.py
+++ models.py
@@ -1,11 +1,27 @@
-import datetime
+import datetime 
 from mongoengine import Document, connect
-from mongoengine import StringField 
+from mongoengine import StringField, ListField, IntField, ReferenceField
 
 DB_NAME = '473'
 
 class Users(Document):
     username = StringField(required=True)
     password = StringField(required=True)
+
+class Course(Document):
+    name = StringField(required=True)
+    number = IntField(required=True)
+    dept = StringField(required=True)
+    prereqs = ListField(StringField())
+
+class Schedule(Document):
+    semester = StringField()
+    year = StringField()
+    user_id = StringField(required=True)
+    courses = ListField(ReferenceField(Course))
+
+class Department(Document):
+    name = StringField(required=True)
+    abbrev = StringField(required=True)
 
 connect(DB_NAME)
