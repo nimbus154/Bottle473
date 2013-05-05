e87968d245e9160df22e8aa4f72b9f770940162f
nimbus154 <nimbus154@gmail.com>
2013-05-05 12:33:14 -0700

    Full ember objects for schedule mock data

--- ember-test/js/app.js
+++ ember-test/js/app.js
@@ -12,20 +12,59 @@ App.Term = Ember.Object.extend({
 	classes: []
 });
 
+App.Class = Ember.Object.extend({
+	name: null,
+	number: 0,
+	dept: null,
+	prereqs: []
+});
+
 var schedules = [
 	App.Schedule.create({
 		year: 2012,
 		terms: [
 			App.Term.create({
 				term: 'Fall',
-				classes: [ 'cpsc223', 'cpsc476', 'cpsc351']
+				classes: [ 
+					App.Class.create({
+						name: 'Python',
+						number: 223,
+						dept: 'cpsc',
+						prereqs: ['cpsc131']
+					}),
+					App.Class.create({
+						name: 'Web Programming',
+						number: 473,
+						dept: 'cpsc',
+						prereqs: ['cpsc433']
+					}),
+					App.Class.create({
+						name: 'Java Enterprise',
+						number: 476,
+						dept: 'cpsc',
+						prereqs: ['cpsc362']
+					})
+				]
 			}),
 			App.Term.create({
 				term: 'Spring',
-				classes: [ 'math150b', 'math270b', 'cpsc362']
+				classes: [ 
+					App.Class.create({
+						name: 'Calculus II',
+						number: '150b',
+						dept: 'math',
+						prereqs: ['math150a']
+					}),
+					App.Class.create({
+						name: 'Math Structures II',
+						number: '270b',
+						dept: 'math',
+						prereqs: ['math270a']
+					})
+				]
 			}),
 			App.Term.create({
-				term: 'Sumer',
+				term: 'Summer',
 				classes: [ 'math365', 'cpsc4eva']
 			})
 		]
@@ -35,26 +74,64 @@ var schedules = [
 		terms: [
 			App.Term.create({
 				term: 'Fall',
-				classes: [ 'cpsc551', 'cpsc433', 'cpsc462']
+				classes: [ 
+					App.Class.create({
+						name: 'Software Engineering',
+						number: 362,
+						dept: 'cpsc',
+						prereqs: ['cpsc131']
+					}),
+					App.Class.create({
+						name: 'Database Programming',
+						number: 431,
+						dept: 'cpsc',
+						prereqs: ['cpsc332']
+					}),
+					App.Class.create({
+						name: 'Programming Languages and Translation',
+						number: 323,
+						dept: 'cpsc',
+						prereqs: ['cpsc240']
+					})
+				]
 			}),
 			App.Term.create({
 				term: 'Spring',
-				classes: [ 'cpsc551', 'cpsc433', 'cpsc462']
+				classes: [ 
+					App.Class.create({
+						name: 'Software Design',
+						number: 462,
+						dept: 'cpsc',
+						prereqs: ['cpsc362']
+					}),
+					App.Class.create({
+						name: 'Data Security and Encryption',
+						number: 433,
+						dept: 'cpsc',
+						prereqs: ['cpsc131']
+					}),
+					App.Class.create({
+						name: 'Distributed Systems',
+						number: 551,
+						dept: 'cpsc',
+						prereqs: ['cpsc351']
+					})
+				]
 			}),
 			App.Term.create({
-				term: 'Sumer',
-				classes: [ 'cpsc335', 'cpsc433']
+				term: 'Summer',
+				classes: [
+					App.Class.create({
+						name: 'Algorithms',
+						number: 335,
+						dept: 'cpsc',
+						prereqs: ['cpsc131']
+					})
+				]
 			})
 		]
 	})
 ];
-
-App.Class = Ember.Object.extend({
-	name: null,
-	number: 0,
-	dept: null,
-	prereqs: []
-});
 
 var classes = [
 	App.Class.create({
@@ -133,7 +210,6 @@ App.DepartmentsRoute = Ember.Route.extend({
 App.ClassesController = Ember.ArrayController.extend({
 	classes: classes,
 	removeClass: function(course) {
-		var removedCourse = this.classes.removeObject(course);
-		console.log(removedCourse);
+		this.classes.removeObject(course);
 	}
 });
