ab8a179ad079fb0da4bcfd94c054ceaece3f04b5
nimbus154 <nimbus154@gmail.com>
2013-05-05 12:19:56 -0700

    Render terms and course list

--- ember-test/js/app.js
+++ ember-test/js/app.js
@@ -2,41 +2,51 @@ var App = Ember.Application.create({
 	LOG_TRANSITIONS: true
 });
 
+App.Schedule = Ember.Object.extend({
+	year: null,
+	terms: [ ]
+});
+
+App.Term = Ember.Object.extend({
+	term: null,
+	classes: []
+});
+
 var schedules = [
-	{
+	App.Schedule.create({
 		year: 2012,
 		terms: [
-			{
+			App.Term.create({
 				term: 'Fall',
 				classes: [ 'cpsc223', 'cpsc476', 'cpsc351']
-			},
-			{
+			}),
+			App.Term.create({
 				term: 'Spring',
 				classes: [ 'math150b', 'math270b', 'cpsc362']
-			},
-			{
+			}),
+			App.Term.create({
 				term: 'Sumer',
 				classes: [ 'math365', 'cpsc4eva']
-			}
+			})
 		]
-	},
-	{
+	}),
+	App.Schedule.create({
 		year: 2013,
 		terms: [
-			{
+			App.Term.create({
 				term: 'Fall',
 				classes: [ 'cpsc551', 'cpsc433', 'cpsc462']
-			},
-			{
+			}),
+			App.Term.create({
 				term: 'Spring',
 				classes: [ 'cpsc551', 'cpsc433', 'cpsc462']
-			},
-			{
+			}),
+			App.Term.create({
 				term: 'Sumer',
 				classes: [ 'cpsc335', 'cpsc433']
-			}
+			})
 		]
-	}
+	})
 ];
 
 App.Class = Ember.Object.extend({
@@ -86,20 +96,26 @@ var departments = [
 
 
 App.Router.map(function() {
+	this.resource('schedule', {path: '/schedule/:year'});
 	this.route('classes');
 	this.route('departments');
 });
 
 App.IndexRoute = Ember.Route.extend({
 	renderTemplate: function() {
-		this.render({ outlet: 'sidebar' });
+		this.transitionTo('schedule', new Date().getFullYear());
+	}
+});
+
+App.ScheduleRoute = Ember.Route.extend({
+	model: function(params) {
+		return schedules.find(function(item) {
+			return item.year == params.year;
+		});
 	}
 });
 
 App.ClassesRoute = Ember.Route.extend({
-	renderTemplate: function() {
-		this.render({ outlet: 'sidebar' });
-	},
 	model: function() {
 		return classes;
 	}
@@ -115,8 +131,9 @@ App.DepartmentsRoute = Ember.Route.extend({
 });
 
 App.ClassesController = Ember.ArrayController.extend({
+	classes: classes,
 	removeClass: function(course) {
-		var removedCourse = classes.removeObject(course);
+		var removedCourse = this.classes.removeObject(course);
 		console.log(removedCourse);
 	}
 });
