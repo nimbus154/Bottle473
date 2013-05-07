450459bf4b1c2421635ac3194c5d42d2b818deec
Robert Cabral <rcabral1990@yahoo.com>
2013-05-06 18:20:06 -0700

    Updated departments.py
    I removed the PUT functions (don't need them, I used them to test)
    /departments will provide a list of departments
    /departments/:abbrev will provide a list of classes

--- departments.py
+++ departments.py
@@ -5,24 +5,14 @@ from pymongo import Connection
 from bson import BSON
 from bson import json_util
  
-connection = Connection('localhost', 27017) #change this to our database, it is local database right now
+connection = Connection('localhost', 27017)
 db = connection.mydatabase
- 
-@route('/departments', method='PUT')    #This is to we can uploaded txt file to database, still need to fix this.
-def put_departments():
-    data = request.body.readline()
-    if not data:
-        abort(400, 'No data received')
-    entity = json.loads(data)
-    try:
-        db['departments'].save(entity)
-    except ValidationError as ve:
-        abort(400, str(ve))
+
      
-@route('/departments', method='GET')   #Get list of all departments
+@route('/departments', method='GET')
 def get_departments():
     departmentslist = []
-    entity = db['departments'].find()
+    entity = db['Department'].find()
     if not entity:
         abort(404, 'No departments available')
     for dp in entity:
@@ -30,11 +20,15 @@ def get_departments():
     return json.dumps(departmentslist, sort_keys=True, indent=4, default=json_util.default)
 
 
-@route('/departments/:abbrev', method='GET')  #Get department information based on department abbreviation
+@route('/departments/:abbrev', method='GET')
 def get_departments_info(abbrev):
-    entity = db['departments'].find_one({'abbrev': abbrev})
+    classlist = []
+    entity = db['Course'].find({'department': abbrev})
     if not entity:
-        abort(404, 'No department information available')
-    return json.dumps(entity, sort_keys=True, indent=4, default=json_util.default)
+        abort(404, 'No classes available')
+    for cl in entity:
+       classlist.append(cl)
+    return json.dumps(classlist, sort_keys=True, indent=4, default=json_util.default)
     
 run(host='localhost', port=8080)
+
